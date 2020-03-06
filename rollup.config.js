efd51d4e979140ea2f3923c747c062d6298c6ecf
Jimseung <lannex.shin@gmail.com>
2020-03-06 18:12:05 +0900

    Setup rollup and ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,26 @@
 import typescript from 'rollup-plugin-typescript2';
+import external from 'rollup-plugin-peer-deps-external';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
 import { uglify } from 'rollup-plugin-uglify';
 
+import pkg from './package.json';
+
 module.exports = {
   input: 'lib/index.tsx',
   output: {
-    file: 'dist/index.js',
+    file: pkg.main,
     format: 'cjs',
+    exports: 'named',
     globals: {
       react: 'React',
     },
   },
-  external: ['react', 'react-dom'],
   plugins: [
+    external(),
     typescript({
+      rollupCommonJSResolveHack: true,
       clean: true,
     }),
     resolve(),
